d2b461560d2a85f47fa45648d03198faa08ab791
km <313976009@qq.com>
2018-12-23 23:23:02 +0800

    大家看下pom.xml里面的jstl和其他包的依赖，有些有冲突

--- src/main/java/Servlet/EvalatorServlet.java
+++ src/main/java/Servlet/EvalatorServlet.java
@@ -9,13 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/EvalatorServlet")
 public class EvalatorServlet extends BaseServlet{
 
-    public String login(HttpServletRequest request, HttpServletResponse response){
+    public String LoadStudentEvalator(HttpServletRequest request, HttpServletResponse response){
+
         System.out.println("hello world login");
         return "/index.jsp";
-    }
-
-    public String exit(HttpServletRequest request, HttpServletResponse response){
-        System.out.println("hello world login");
-        return "/html/courseDetail-4.html";
     }
 }
