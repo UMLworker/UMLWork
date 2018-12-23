397e53e84ec347e1936f2a9267106f71e59c7ef6
km <313976009@qq.com>
2018-12-23 17:47:50 +0800

    增加了网上评教的servlet，dao和domain

---
+++ src/main/java/Domain/Online_Evalator.java
@@ -0,0 +1,58 @@
+package Domain;
+
+public class Online_Evalator {
+    private String course_teacher_name;
+    private String course_time;
+    private String course_name;
+    private String course_property;
+    private String course_grade;
+    private String course_level;
+
+    public String getCourse_teacher_name() {
+        return course_teacher_name;
+    }
+
+    public void setCourse_teacher_name(String course_teacher_name) {
+        this.course_teacher_name = course_teacher_name;
+    }
+
+    public String getCourse_time() {
+        return course_time;
+    }
+
+    public void setCourse_time(String course_time) {
+        this.course_time = course_time;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_property() {
+        return course_property;
+    }
+
+    public void setCourse_property(String course_property) {
+        this.course_property = course_property;
+    }
+
+    public String getCourse_grade() {
+        return course_grade;
+    }
+
+    public void setCourse_grade(String course_grade) {
+        this.course_grade = course_grade;
+    }
+
+    public String getCourse_level() {
+        return course_level;
+    }
+
+    public void setCourse_level(String course_level) {
+        this.course_level = course_level;
+    }
+}
